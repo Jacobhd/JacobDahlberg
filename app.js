1c62a3d217fe78eccf250fdd4f11f5258573357e
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2021-03-29 13:48:23 -0600

    Summary
    Starting server attempts

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 const express = require('express');
-
+const pug = require('pug');
+const data = require('./data.json');
+const port = 3000;
 const app = express();
 
-app.listen(3000);
+app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', data);
+});
+
+app.get('/about', function (req, res) {
+  res.render('index', data);
+});
+
+app.get('/projects/:id', function (req, res) {
+  let id = req.params.id;
+  let project = data.Projects[id];
+  if(project) {
+    res.render('project',project);
+  } else {
+    res.redirect('/404');
+  }
+})
+
+//const mainRoutes = require('./routes');
+//const cardRoutes = require('./routes/cards');
+
+//app.use(mainRoutes);
+//app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+app.listen(port, () => {
+    console.log(`The application is running on localhost:${port}!`);
+});
