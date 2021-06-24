da27b50e4cc1132a48e6133265d23873692ef472
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2021-06-24 14:41:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use((err, req, res, next) => {
 });
 
 /* PORT */
-app.listen(process.env.PORT || 3000, () => { 
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port 3000");
 });
