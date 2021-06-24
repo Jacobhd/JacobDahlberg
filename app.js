2a88dbc7afa909984f7dbb22bd435b047f3d9b21
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2021-06-24 13:58:20 -0700

    Update Port for Heroku Deploy

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use((err, req, res, next) => {
 });
 
 /* PORT */
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000');
+app.listen(process.env.PORT || 3000, () => { 
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
 });
